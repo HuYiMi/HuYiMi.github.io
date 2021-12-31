74f38e9db9de4bcab672edf45aa53c54cab8ec3a
LiYeZhiXiu <3201534466@qq.com>
2021-12-31 17:54:05 +0800

    Site updated: 2021-12-31 17:53:59

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+  "CONFIG":
+  {
+    "modules": "",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "right": "10",
+    "top": "80",
+    "key": "2a740296f0354c76bb9f743d08452c35"
+  }
+}
